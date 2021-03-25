eb879ac336f8d060c703c6f31398722eb0c0375f
iqbal <iqballutfian30@gmail.com>
2021-03-25 22:37:50 +0700

    belum kelar

---
+++ W2/a Movable Circles/src/CopyingaFile.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CopyingaFile {
+	public static void main(String args[]) {
+		String fileName;
+
+		System.out.println("Enter file name:");
+		
+		
+}
